ddcfce9ec0975981d446eecba50f21cf8392ce8e
MikaRinquest <rinquestmika@gmail.com>
2023-01-20 15:18:54 +0200

    added dar template

--- routes/emailingRoute.js
+++ routes/emailingRoute.js
@@ -1,37 +1,20 @@
 const express = require("express");
 const router = express.Router();
-const con = require("../library/db_connection");
+const fs = require("fs");
+const handlebars = require("handlebars");
 const nodemailer = require("nodemailer");
 
 // Send DAR info
 router.post("/", (req, res) => {
   try {
-    const {
-      damage,
-      facility,
-      severity,
-      category,
-      method,
-      recommendation,
-      internalLocations,
-      externalLocations,
-      concealed,
-      notConcealed,
-      remarks,
-    } = req.body;
-
-    const dar = {
-      damage,
-      facility,
-      severity,
-      category,
-      method,
-      recommendation,
-      internalLocations,
-      externalLocations,
-      concealed,
-      notConcealed,
-      remarks,
+    const readHTMLFile = function (path, callback) {
+      fs.readFile(path, { encoding: "utf-8" }, function (err, html) {
+        if (err) {
+          callback(err);
+        } else {
+          callback(null, html);
+        }
+      });
     };
 
     const transporter = nodemailer.createTransport({
@@ -41,41 +24,62 @@ router.post("/", (req, res) => {
         pass: process.env.MAILER_PASS, //Accessing the password in dotenv
       },
     });
-    const mailData = {
-      from: process.env.MAILER_USER,
-      to: "rinquestmika@gmail.com",
-      subject: "Sending DAR information",
-      text: ` 
-      <html>
-      <div>
-      <p>Damage Type:${dar.damage}</p>
-      <p>Facility:${dar.facility}</p>
-      <p>Leak/Damage Severity:${dar.severity}</p>
-      <p>Inspection Category:${dar.category}</p>
-      <p>Leak Detection Method:${dar.method}</p>
-      <p>Repair Action Recommended:${dar.recommendation}</p>
-      <p>Internal Leak/Damage Location:${dar.internalLocations}</p>
-      <p>External Leak/Damage Location:${dar.externalLocations}</p>
-      <p>Concealed Leak/Damage Status:${dar.concealed}</p>
-      <p>Not Concealed Leak/Damage Status:${dar.notConcealed}</p>
-      <p>Remarks:${dar.remarks}</p>
-      </div>
-      </html>
-      `,
-    };
-    transporter.verify((error, success) => {
-      if (error) {
-        console.log(error);
-      } else {
-        console.log("Email valid", success);
+
+    readHTMLFile(__dirname + "", function (err, html) {
+      if (err) {
+        console.log("error reading file", err);
+        return;
       }
-    });
-    transporter.sendMail(mailData, (error, info) => {
-      if (error) {
-        console.log(error);
-      } else {
-        res.send("Please check your emails");
-      }
+      const template = handlebars.compile(html);
+      const {
+        damage,
+        facility,
+        severity,
+        category,
+        method,
+        recommendation,
+        internalLocations,
+        externalLocations,
+        concealed,
+        notConcealed,
+        remarks,
+      } = req.body;
+
+      const dar = {
+        damage,
+        facility,
+        severity,
+        category,
+        method,
+        recommendation,
+        internalLocations,
+        externalLocations,
+        concealed,
+        notConcealed,
+        remarks,
+      };
+      const htmlToSend = template(dar);
+
+      const mailData = {
+        from: process.env.MAILER_USER,
+        to: "rinquestmika@gmail.com",
+        subject: "Sending DAR information",
+        text: htmlToSend,
+      };
+      transporter.verify((error, success) => {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log("Email valid", success);
+        }
+      });
+      transporter.sendMail(mailData, (error, info) => {
+        if (error) {
+          console.log(error);
+        } else {
+          res.send("Please check your emails");
+        }
+      });
     });
   } catch (error) {
     console.log(error);
